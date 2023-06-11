cd2356a9fb20751af2ddf7c6ca84e7e8a4b23ca5
Stevious <124934062+stevious-business@users.noreply.github.com>
2023-06-12 00:23:58 +0200

    Add more MLF cases to /tests

---
+++ tests/test_MLF_complex_2.c
@@ -0,0 +1,28 @@
+#include"../lib/scheduler.h"
+#include"../lib/process.h"
+#include "helpers.h"
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+#define PROCESS_COUNT   4
+#define RR_QUANTUM      2
+#define STRATEGY        MLF
+
+int main()
+{
+    /*Defining the processes                Arrival Dur.    Prio    ID
+                                            uint    uint    uint    char*/
+    process processes[PROCESS_COUNT] = {{   0,      5,      1,      'A'},
+                                        {   3,      3,      4,      'B'},
+                                        {   4,      15,      2,      'C'},
+                                        {   8,      11,      3,      'D'}};
+    char* expected_result = "AAABCBBCCDDDAACCCCCCDDDDDDCCCCCCDD";
+    print_schedule_info(PROCESS_COUNT, STRATEGY, 0, processes);
+
+    char* resulting_schedule = scheduler(processes,PROCESS_COUNT,STRATEGY,RR_QUANTUM);
+
+    unsigned int err = compare_result(resulting_schedule, expected_result);
+    free(resulting_schedule);
+    exit(err);
+}
